462285dd0feff363971c66de2fb544e29119f573
alifrasyid <alifrasyid.271003@gmail.com>
2024-04-24 02:26:26 +0700

    Large class
    memisahkan beberapa variabel ke dalam class-class

---
+++ src/lib/SalaryInfo.java
@@ -0,0 +1,28 @@
+package lib;
+
+public class SalaryInfo {
+
+    private int monthlySalary;
+    private int otherMonthlyIncome;
+    private int annualDeductible;
+
+    public int getMonthlySalary() {
+        return monthlySalary;
+    }
+    public void setMonthlySalary(int monthlySalary) {
+        this.monthlySalary = monthlySalary;
+    }
+    public int getOtherMonthlyIncome() {
+        return otherMonthlyIncome;
+    }
+    public void setOtherMonthlyIncome(int otherMonthlyIncome) {
+        this.otherMonthlyIncome = otherMonthlyIncome;
+    }
+    public int getAnnualDeductible() {
+        return annualDeductible;
+    }
+    public void setAnnualDeductible(int annualDeductible) {
+        this.annualDeductible = annualDeductible;
+    }
+    
+}
